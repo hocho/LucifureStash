b958844a4098aa11414ba31597e73f0492876797
hocho_cp <hocho@users.noreply.github.com>
2013-01-27 05:10:06 +0000

    Added IgnoreResourceNotFoundException

---
+++ Source/Lucifure.Stash.Test/Exception Test Run Time/IgnoreResourceNotFoundException.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using CodeSuperior.Lucifure;
+
+namespace Lucifure.Stash.Test.ExceptionsRT
+{
+	// -----------------------------------------------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------------------------
+
+	public 
+	partial 
+	class TestForExceptionsRT 
+	{
+		void
+		IgnoreResouceNotFoundExceptionImpl(
+			bool								ignoreResouceNotFoundException) 
+		{
+			StashClient<KeyDataExplicit>		clientWithExtra = null;
+			KeyDataExplicit						dataWritten = null;
+
+			try
+			{
+				clientWithExtra = StashConfiguration.GetClient<KeyDataExplicit>();		
+			
+				clientWithExtra.CreateTableIfNotExist();
+
+				var
+				clientMin = StashConfiguration.GetClient<KeyDataExplicit>(
+									new StashClientOptions {
+											IgnoreResourceNotFoundException = ignoreResouceNotFoundException,
+											OverrideEntitySetName = instance => typeof(KeyDataExplicit).Name,
+											OverrideEntitySetNameIsDynamic = false,
+										});		
+				
+				dataWritten = new KeyDataExplicit {
+												PartitionKey = "ResourceNotFound",
+												RowKey = Guid.NewGuid().ToString() };
+
+				clientWithExtra.Insert(dataWritten);
+
+				var
+				dataRead = clientMin.Get(
+										dataWritten.PartitionKey, 
+										dataWritten.RowKey + "X");
+
+				// we should reach here only if ignoreResouceNotFoundException is true
+				Assert.IsTrue(ignoreResouceNotFoundException);
+				Assert.IsTrue(dataRead == null);
+			}
+			catch (StashException stashEx)
+			{
+				Assert.IsTrue(
+					!ignoreResouceNotFoundException 
+						&& stashEx.Error == StashError.UnexpectedRuntime);
+			}
+			catch (Exception ex)
+			{
+				Assert.Fail();
+			}
+
+			clientWithExtra.Delete(dataWritten);
+		}
+
+		[TestMethod]
+		public 
+		void 
+		IgnoreResouceNotFoundException()
+		{
+			IgnoreResouceNotFoundExceptionImpl(true);	
+			IgnoreResouceNotFoundExceptionImpl(false);	
+		} 
+	}
+
+	// -----------------------------------------------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------------------------
+}
