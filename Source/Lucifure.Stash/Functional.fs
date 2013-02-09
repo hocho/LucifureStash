bf7d188eb3f85d38a360abbb9db85a9888bba734
hocho_cp <SND\hocho_cp>
2013-02-09 23:04:59 +0000

    Added thread safe dictionary and used it in StashContext.

--- Source/Lucifure.Stash/Functional.fs
+++ Source/Lucifure.Stash/Functional.fs
@@ -3,6 +3,8 @@
 namespace CodeSuperior.Common
 
 open System
+open System.Linq
+open System.Threading
 open System.Collections
 open System.Collections.Generic
 
@@ -79,3 +81,95 @@ type IEnumeratorWrapper<'a>
                 let o = inner.Current
                 callback o
                 o
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+
+type SafeReaderWriter() = 
+
+    let lock = new ReaderWriterLockSlim()
+
+    member this.Read f = 
+
+        lock.EnterReadLock()
+
+        try
+            f()
+        finally 
+            lock.ExitReadLock()
+
+    member this.Write f = 
+
+        lock.EnterWriteLock()
+        
+        try
+            f()
+        finally 
+            lock.ExitWriteLock()
+
+    
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+
+type DictionarySafe<'k, 'v>  when 'k : equality () = 
+   
+    let dict = new Dictionary<'k, 'v>()
+
+    let safe = new SafeReaderWriter()
+
+    // writers
+
+    member this.Put key value = 
+
+        fun () -> dict.[key] <- value
+
+        |> safe.Write
+
+    member this.Remove key = 
+
+        (fun () -> dict.Remove key |> ignore) 
+
+        |> safe.Write
+
+    member this.Clear key = 
+
+        (fun () -> dict.Clear() |> ignore) 
+
+        |> safe.Write
+
+    // readers
+
+    member this.Get key = 
+
+        fun () -> dict.[key]
+
+        |> safe.Read
+
+    member this.SafeGet key = 
+
+        (fun () -> 
+        match dict.TryGetValue key with
+        |   true, v     ->  v
+        |   _           ->  Unchecked.defaultof<'v>)
+
+        |> safe.Read
+
+    member this.ContainsKey key = 
+
+        fun () -> dict.ContainsKey key 
+
+        |> safe.Read
+
+    member this.Keys = 
+
+        fun () -> dict.Keys.ToList()
+
+        |> safe.Read
+
+    member this.Values = 
+
+        fun () -> dict.Values.ToList()
+
+        |> safe.Read
+
